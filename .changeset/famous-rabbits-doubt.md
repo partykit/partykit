4e4f271dfb384fd01b6b57532b0b4f2b21329b71
Sunil Pai <threepointone@gmail.com>
2022-12-31 14:23:19 +0000

    `export default {onConnect}`
    changing the server's export signature so it's easier to add types and validate.

---
+++ .changeset/famous-rabbits-doubt.md
@@ -0,0 +1,7 @@
+---
+"partykit": patch
+---
+
+`export default {onConnect}`
+
+changing the server's export signature so it's easier to add types and validate.
--- examples/basic/src/server.ts
+++ examples/basic/src/server.ts
@@ -1,11 +1,12 @@
-export function onConnect(ws: WebSocket) {
-  ws.onmessage = function incoming(evt) {
-    console.log("evt!", evt.data);
-    ws.send("pong");
-  };
-}
-
-// this doesn't work just yet...
-// export function onRequest(_req: Request) {
-//   return new Response("Yoohoo from the room");
-// }
+export default {
+  onConnect(ws: WebSocket) {
+    ws.onmessage = function incoming(evt) {
+      console.log("evt!", evt.data);
+      ws.send("pong");
+    };
+  },
+  // this doesn't work just yet...
+  // onRequest(_req: Request) {
+  //   return new Response("Yoohoo from the room");
+  // }
+};
