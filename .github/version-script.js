5c87f01f99b2662d0af2abf81e6a6fb3d69de626
Sunil Pai <threepointone@gmail.com>
2023-01-08 00:33:06 +0000

    manually publish prereleases
    This reverts part of commit 1e252b5da31a4e1d0f053ba960085fcd7a303718. Looks like the changesets action doesn't care about snapshot releases. But let's publish beta versions of y-partykit anyway.

---
+++ .github/version-script.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const { exec } = require("child_process");
+
+try {
+  exec("git rev-parse --short HEAD", (err, stdout) => {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+    for (const path of [
+      "./packages/partykit/package.json",
+      "./packages/y-partykit/package.json",
+    ]) {
+      const package = JSON.parse(fs.readFileSync(path));
+      package.version = "0.0.0-" + stdout.trim();
+      fs.writeFileSync(path, JSON.stringify(package, null, "\t") + "\n");
+    }
+  });
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
