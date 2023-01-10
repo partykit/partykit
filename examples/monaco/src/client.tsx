f7d78ef3e74013d3d8df841fe29846fca040f4de
Sunil Pai <threepointone@gmail.com>
2023-01-10 21:43:15 +0000

    add an example for monaco editor + y-partykit (#23)

---
+++ examples/monaco/src/client.tsx
@@ -0,0 +1,63 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-env browser */
+
+import * as Y from "yjs";
+import YPartyKitProvider from "y-partykit/provider";
+import { MonacoBinding } from "y-monaco";
+import * as monaco from "monaco-editor";
+
+self.MonacoEnvironment = {
+  getWorkerUrl: function (moduleId, label) {
+    if (label === "json") {
+      // return "/dist/json.worker.bundle.js";
+      return "/dist/monaco-editor/esm/vs/language/json/json.js";
+    }
+    if (label === "css" || label === "scss" || label === "less") {
+      return "/dist/monaco-editor/esm/vs/language/css/css.js";
+      // return "/dist/css.worker.bundle.js";
+    }
+    if (label === "html" || label === "handlebars" || label === "razor") {
+      return "/dist/monaco-editor/esm/vs/language/html/html.js";
+      // return "/dist/html.worker.bundle.js";
+    }
+    if (label === "typescript" || label === "javascript") {
+      // return "/dist/ts.worker.bundle.js";
+      return "/dist/monaco-editor/esm/vs/language/typescript/ts.js";
+    }
+    return "/dist/monaco-editor/esm/vs/editor/editor.worker.js";
+    // return "/dist/editor.worker.bundle.js";
+  },
+};
+
+window.addEventListener("load", () => {
+  const ydoc = new Y.Doc();
+  const provider = new YPartyKitProvider("localhost:1999", "monaco", ydoc);
+  const type = ydoc.getText("monaco");
+
+  const editor = monaco.editor.create(
+    /** @type {HTMLElement} */ document.getElementById("monaco-editor")!,
+    {
+      value: "",
+      language: "javascript",
+      theme: "vs-dark",
+    }
+  );
+  new MonacoBinding(
+    type,
+    /** @type {monaco.editor.ITextModel} */ editor.getModel()!,
+    new Set([editor]),
+    provider.awareness
+  );
+
+  const connectBtn =
+    /** @type {HTMLElement} */ document.getElementById("y-connect-btn")!;
+  connectBtn.addEventListener("click", () => {
+    if (provider.shouldConnect) {
+      provider.disconnect();
+      connectBtn.textContent = "Connect";
+    } else {
+      provider.connect();
+      connectBtn.textContent = "Disconnect";
+    }
+  });
+});
