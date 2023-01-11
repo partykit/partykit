670e2888d26e2e49159ca1b0870daa274669ea43
Sunil Pai <threepointone@gmail.com>
2023-01-11 15:26:12 +0000

    usePartySocket (#24)
    import usePartySocket from "partysocket/react";

--- packages/partysocket/src/react.ts
+++ packages/partysocket/src/react.ts
@@ -1,8 +1,21 @@
 // import React from "react";
 // import PartySocket from ".";
 import type { PartySocketOptions } from ".";
+import PartySocket from ".";
+import { useRef, useEffect } from "react";
 
 // A React hook that wraps PartySocket
-export default function usePartySocket(_options: PartySocketOptions) {
-  // todo: implement the thing
+export default function usePartySocket(options: PartySocketOptions) {
+  const socketRef = useRef<PartySocket>(
+    new PartySocket({
+      ...options,
+      startClosed: true,
+    })
+  );
+  useEffect(() => {
+    socketRef.current.reconnect();
+    return () => {
+      socketRef.current.close();
+    };
+  }, []);
 }
