5be70b2d12b870e7cce40e6e5774db20e007e6ac
Sunil Pai <threepointone@gmail.com>
2023-01-16 23:49:24 +0000

    feat: persistence / storage (phase 1)
    This implements persistence / storage for partykit. It hijack's cloudflare's DO storage api (without config options). This doesn't implement DO's i/o gates yet, but that's kinda fine, because it means you have to write code that's good in dev (but production will automatically be better). We'll implement them later. Also this currently does in-memory storage. We'll fix that in the future by (optionally) using disk for persistence.

--- packages/partykit/src/server.ts
+++ packages/partykit/src/server.ts
@@ -6,13 +6,35 @@ export type PartyKitConnection = {
   unstable_initial: unknown;
 };
 
+export type PartyKitStorage = {
+  get<T = unknown>(key: string): Promise<T | undefined>;
+  get<T = unknown>(keys: string[]): Promise<Map<string, T>>;
+  list<T = unknown>(options?: {
+    start?: string;
+    startAfter?: string;
+    end?: string;
+    prefix?: string;
+    reverse?: boolean;
+    limit?: number;
+  }): Promise<Map<string, T>>;
+  put<T>(key: string, value: T): Promise<void>;
+  put<T>(entries: Record<string, T>): Promise<void>;
+  delete(key: string): Promise<boolean>;
+  delete(keys: string[]): Promise<number>;
+  deleteAll(): Promise<void>;
+  // getAlarm(): Promise<number | null>;
+  // setAlarm(scheduledTime: number | Date): Promise<void>;
+  // deleteAlarm(): Promise<void>;
+};
+
 export type PartyKitRoom = {
   id: string; // room id, usually a slug
   connections: Map<string, PartyKitConnection>;
   env: Record<string, string>; // use a .env file, or --var
+  storage: PartyKitStorage;
 };
 
-export type PartyKitServer = {
+export type PartyKitServer<Initial = unknown> = {
   onConnect: (ws: WebSocket, room: PartyKitRoom) => void;
-  onBeforeConnect?: (req: Request) => Promise<unknown>;
+  onBeforeConnect?: (req: Request) => Promise<Initial>;
 };
