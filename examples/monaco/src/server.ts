564a48c569a80ccf2a7eb584740f8b17581a5ec4
Sunil Pai <threepointone@gmail.com>
2023-02-02 16:53:10 +0000

    remove y-partykit's onCommand config
    now that we have onRequest, we don't need to set up a command channel within y-partykit itself, so let's remove the code for it.

--- examples/monaco/src/server.ts
+++ examples/monaco/src/server.ts
@@ -4,17 +4,6 @@ export default {
   onConnect(ws, room) {
     onConnect(ws, room, {
       persist: true,
-      onCommand(command, _doc) {
-        switch (command) {
-          case "do-the-thing": {
-            console.log("we did the thing");
-            break;
-          }
-          default: {
-            console.warn("unrecognised message");
-          }
-        }
-      },
     });
   },
 } satisfies PartyKitServer;
