94a4b032fbdce8da7dd7e724805d85fa6bb324b2
Anders Bech Mellson <mellson@icloud.com>
2023-05-09 17:27:44 +0200

    Use tsup for partysocket and y-partykit

---
+++ packages/partysocket/createSymlinks.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+ln -sf dist/ws.d.ts ws.d.ts
+ln -sf dist/react.d.ts react.d.ts
---
+++ packages/y-partykit/createSymlinks.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+ln -sf dist/provider.d.ts provider.d.ts
+ln -sf dist/storage.d.ts storage.d.ts
