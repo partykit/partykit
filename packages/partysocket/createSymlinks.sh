8e059547c869fe4a14a1afb897ac03a33fdac70d
Anders Bech Mellson <mellson@icloud.com>
2023-05-09 18:52:19 +0200

    Handle declaration files manually

--- packages/partysocket/createSymlinks.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-ln -sf dist/ws.d.ts ws.d.ts
-ln -sf dist/react.d.ts react.d.ts
--- packages/y-partykit/createSymlinks.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-ln -sf dist/provider.d.ts provider.d.ts
-ln -sf dist/storage.d.ts storage.d.ts
