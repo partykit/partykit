29ced78cd95bd22005d54d43a51a6b5fe91b28d6
Yousef <yousefdardiry@gmail.com>
2023-05-23 22:50:10 +0200

    blocknote demo (#139)

---
+++ examples/blocknote/src/randomUser.ts
@@ -0,0 +1,39 @@
+const colors = [
+  "#958DF1",
+  "#F98181",
+  "#FBBC88",
+  "#FAF594",
+  "#70CFF8",
+  "#94FADB",
+  "#B9F18D",
+];
+
+// some random gpt generated names
+const names = [
+  "Lorem Ipsumovich",
+  "Typy McTypeface",
+  "Collabo Rative",
+  "Edito Von Editz",
+  "Wordsworth Writywrite",
+  "Docu D. Mentor",
+  "Scrivener Scribblesworth",
+  "Digi Penman",
+  "Ernest Wordway",
+  "Sir Typalot",
+  "Comic Sans-Serif",
+  "Miss Spellcheck",
+  "Bullet Liston",
+  "Autonomy Backspace",
+  "Ctrl Zedson",
+];
+
+const getRandomElement = <T>(list: T[]) =>
+  list[Math.floor(Math.random() * list.length)];
+
+const getRandomColor = () => getRandomElement(colors);
+const getRandomName = () => getRandomElement(names);
+
+export const getRandomUser = () => ({
+  name: getRandomName(),
+  color: getRandomColor(),
+});
