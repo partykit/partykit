f81f4544e4462a0e69f6e18955ac19a73084a9d7
yousefed <yousefdardiry@gmail.com>
2023-05-23 22:39:09 +0200

    blocknote demo

---
+++ examples/blocknote/src/client.tsx
@@ -0,0 +1,39 @@
+/** @jsx React.createElement */
+
+import * as React from "react";
+import YPartyKitProvider from "y-partykit/provider";
+import * as Y from "yjs";
+import { createRoot } from "react-dom/client";
+import { BlockNoteView, useBlockNote } from "@blocknote/react";
+import "@blocknote/core/style.css";
+import { getRandomUser } from "./randomUser";
+
+declare const PARTYKIT_HOST: string | undefined;
+
+const partykitHost =
+  typeof PARTYKIT_HOST === "undefined" ? "localhost:1999" : PARTYKIT_HOST;
+
+const doc = new Y.Doc();
+const provider = new YPartyKitProvider(
+  partykitHost || "localhost:1999",
+  "my-document-id",
+  doc
+);
+
+function Editor() {
+  const editor = useBlockNote({
+    collaboration: {
+      // The Yjs Provider responsible for transporting updates:
+      provider,
+      // Where to store BlockNote data in the Y.Doc:
+      fragment: doc.getXmlFragment("document-store"),
+      // Information (name and color) for this user:
+      user: getRandomUser(),
+    },
+  });
+  return <BlockNoteView editor={editor} />;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(document.getElementById("root")!);
+root.render(<Editor />);
