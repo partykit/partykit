6e2b0646e63c47c667dcd362a04acec495e753b3
Tom Sherman <the.tomsherman@gmail.com>
2023-07-16 20:51:28 +0100

    Refactor partysocket to use native EventTarget (#172)
    * Refactor to use native EventTarget
    
    * Clone event so that we can dispatch it

---
+++ packages/partysocket/src/type-helper.ts
@@ -0,0 +1,20 @@
+export type TypedEventTarget<EventMap extends object> = {
+  new (): IntermediateEventTarget<EventMap>;
+};
+
+// internal helper type
+interface IntermediateEventTarget<EventMap> extends EventTarget {
+  addEventListener<K extends keyof EventMap>(
+    type: K,
+    callback: (
+      event: EventMap[K] extends Event ? EventMap[K] : never
+    ) => EventMap[K] extends Event ? void : never,
+    options?: boolean | AddEventListenerOptions
+  ): void;
+
+  addEventListener(
+    type: string,
+    callback: EventListenerOrEventListenerObject | null,
+    options?: EventListenerOptions | boolean
+  ): void;
+}
