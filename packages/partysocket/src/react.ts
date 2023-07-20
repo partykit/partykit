703d4522d34bfa4452d8cdc7ddb156ef2c467600
Jani Eväkallio <jani.evakallio@gmail.com>
2023-07-20 14:26:55 +0100

    Add websocket lifecycle event handlers to usePartyKit options to reduce need for effects in userland

--- packages/partysocket/src/react.ts
+++ packages/partysocket/src/react.ts
@@ -4,24 +4,51 @@ import { useEffect, useRef } from "react";
 import type { PartySocketOptions } from ".";
 import PartySocket from ".";
 
+type UsePartySocketOptions = PartySocketOptions & {
+  onOpen?: (event: WebSocketEventMap["open"]) => void;
+  onMessage?: (event: WebSocketEventMap["message"]) => void;
+  onClose?: (event: WebSocketEventMap["close"]) => void;
+  onError?: (event: WebSocketEventMap["error"]) => void;
+};
+
 // A React hook that wraps PartySocket
-export default function usePartySocket(options: PartySocketOptions) {
+export default function usePartySocket(options: UsePartySocketOptions) {
+  const { onOpen, onMessage, onClose, onError, ...partySocketOptions } =
+    options;
+
   const socketRef = useRef<PartySocket>(
     new PartySocket({
-      ...options,
+      ...partySocketOptions,
       startClosed: true, // only connect on mount
     })
   );
 
+  // note: this effect must be defined before the auto-connect effect below
+  useEffect(() => {
+    const socket = socketRef.current;
+    if (onOpen) socket.addEventListener("open", onOpen);
+    if (onClose) socket.addEventListener("close", onClose);
+    if (onError) socket.addEventListener("error", onError);
+    if (onMessage) socket.addEventListener("message", onMessage);
+
+    return () => {
+      if (onOpen) socket.removeEventListener("open", onOpen);
+      if (onClose) socket.removeEventListener("close", onClose);
+      if (onError) socket.removeEventListener("error", onError);
+      if (onMessage) socket.removeEventListener("message", onMessage);
+    };
+  }, [onOpen, onMessage, onClose, onError]);
+
+  // note: this effect must be defined after the event listener registration above
   useEffect(
     () => {
+      const socket = socketRef.current;
       if (options.startClosed !== true) {
-        socketRef.current.reconnect();
+        socket.reconnect();
       }
 
       return () => {
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        socketRef.current.close();
+        socket.close();
       };
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
