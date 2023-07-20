39cf5cebf5e699bc50ace8b6d25cd82c807e863a
Jani Eväkallio <jani.evakallio@gmail.com>
2023-07-20 14:33:36 +0100

    Improve PartySocket types and React hooks API (#191)
    * Fix startClosed: Allow usePartySocket without opening connection
    
    * Fix types for PartySocket#removeEventListener
    
    * Add websocket lifecycle event handlers to usePartyKit options to reduce need for effects in userland
    
    * Changeset

--- packages/partysocket/src/type-helper.ts
+++ packages/partysocket/src/type-helper.ts
@@ -17,4 +17,18 @@ interface IntermediateEventTarget<EventMap> extends EventTarget {
     callback: EventListenerOrEventListenerObject | null,
     options?: EventListenerOptions | boolean
   ): void;
+
+  removeEventListener<K extends keyof EventMap>(
+    type: K,
+    callback: (
+      event: EventMap[K] extends Event ? EventMap[K] : never
+    ) => EventMap[K] extends Event ? void : never,
+    options?: boolean | AddEventListenerOptions
+  ): void;
+
+  removeEventListener(
+    type: string,
+    callback: EventListenerOrEventListenerObject | null,
+    options?: EventListenerOptions | boolean
+  ): void;
 }
