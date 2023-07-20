8f48837e4e79de442674e829a214cf7457e96170
Jani Eväkallio <jani.evakallio@gmail.com>
2023-07-20 14:08:21 +0100

    Fix startClosed: Allow usePartySocket without opening connection

--- packages/partysocket/src/react.ts
+++ packages/partysocket/src/react.ts
@@ -9,15 +9,23 @@ export default function usePartySocket(options: PartySocketOptions) {
   const socketRef = useRef<PartySocket>(
     new PartySocket({
       ...options,
-      startClosed: true,
+      startClosed: true, // only connect on mount
     })
   );
-  useEffect(() => {
-    socketRef.current.reconnect();
-    return () => {
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      socketRef.current.close();
-    };
-  }, []);
+
+  useEffect(
+    () => {
+      if (options.startClosed !== true) {
+        socketRef.current.reconnect();
+      }
+
+      return () => {
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        socketRef.current.close();
+      };
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
   return socketRef.current;
 }
