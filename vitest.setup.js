d4be0883681a4eb92c88cf53743d69c7074e0d52
Jani Eväkallio <jani.evakallio@gmail.com>
2023-07-31 15:36:35 +0100

    Configure dashboard server url

--- vitest.setup.js
+++ vitest.setup.js
@@ -1,2 +1,4 @@
 // eslint-disable-next-line no-undef
 globalThis.PARTYKIT_API_BASE = "fuck.partykit.dev";
+// eslint-disable-next-line no-undef
+globalThis.PARTYKIT_DASHBOARD_BASE = "dashboard.partykit.io";
