12a1ab5afe1991f121da954223ecf13e23c0128b
Jani Eväkallio <jani.evakallio@gmail.com>
2023-08-16 17:57:37 +0100

    Clarifying comments

--- packages/partykit/src/auth/clerk.ts
+++ packages/partykit/src/auth/clerk.ts
@@ -12,8 +12,13 @@ type TokenStore = {
 const CLERK_PUBLISHABLE_KEY = "pk_live_Y2xlcmsucGFydHlraXQuaW8k";
 
 const clerkFactory = ({ publishableKey }: { publishableKey: string }) => {
+  /**
+   * The Clerk CLI is meant for browser use, and relies on cookies by default.
+   * Here we make it work by passing the token in the Authorization header instead.
+   */
   return async ({ tokenStore }: { tokenStore: TokenStore }): Promise<Clerk> => {
     const clerk: Clerk = new ClerkConstructor(publishableKey);
+    // pass captured or provided token in subsequent requests
     clerk.__unstable__onBeforeRequest(async (requestInit) => {
       requestInit.credentials = "omit";
       requestInit.url?.searchParams.append("_is_native", "1");
@@ -23,6 +28,7 @@ const clerkFactory = ({ publishableKey }: { publishableKey: string }) => {
       );
     });
 
+    // capture token from responses for future use
     clerk.__unstable__onAfterResponse(async (_, response) => {
       const authHeader = response?.headers.get("authorization");
       if (authHeader) {
@@ -41,6 +47,7 @@ export const createClerkClient = clerkFactory({
 });
 
 export const fetchClerkClientToken = async (signInToken: string) => {
+  // the login process will populate the token in this object
   const tokenStore = {
     token: undefined,
   };
