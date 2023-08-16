6c36678415d3ffd3e753296ace660dccd84fe62e
gregs <ukfaeku@gmail.com>
2023-08-16 14:05:40 -0300

    provider-less mode

--- packages/partysocket/src/react.tsx
+++ packages/partysocket/src/react.tsx
@@ -11,58 +11,101 @@ import React, {
 
 const PartySocketContext = createContext<PartySocket | null>(null);
 
-export function PartySocketProvider({
-  children,
-  ...options
-}: PropsWithChildren<PartySocketOptions>) {
-  const [socket] = useState(
-    () => new PartySocket({ startClosed: true, ...options })
-  );
+type UsePartySocketListeners = {
+  onOpen?: (event: WebSocketEventMap["open"]) => void;
+  onMessage?: (event: WebSocketEventMap["message"]) => void;
+  onClose?: (event: WebSocketEventMap["close"]) => void;
+  onError?: (event: WebSocketEventMap["error"]) => void;
+};
+type UsePartySocketOptions =
+  | (PartySocketOptions & UsePartySocketListeners)
+  | (UsePartySocketListeners & { room?: never; host?: never });
 
-  useEffect(() => {
-    if (options.startClosed !== true) socket.reconnect();
-    return () => {
-      socket.close();
-    };
-  }, []);
+const globalPartySockets: {
+  [host: string]: {
+    [room: string]: PartySocket;
+  };
+} = {};
+const listenersCounter: { [host: string]: { [room: string]: number } } = {};
+const getGlobalPartySocket = (options?: PartySocketOptions) => {
+  if (!options) return;
 
-  return (
-    <PartySocketContext.Provider value={socket}>
-      {children}
-    </PartySocketContext.Provider>
-  );
-}
+  const { host, room } = options;
+  globalPartySockets[host] ??= {};
+  globalPartySockets[host][room] ??= new PartySocket({
+    ...options,
+    startClosed: true,
+  });
 
-export const usePartySocket = () => {
-  const socket = useContext(PartySocketContext);
-  if (!socket)
-    throw new Error("usePartySocket must be used within a PartySocketProvider");
-  return socket;
+  return globalPartySockets[host][room];
 };
 
-export const usePartySocketEvent = <
+const usePartySocketEvent = <
   E extends keyof WebSocketEventMap,
   CbArgs extends WebSocketEventMap[E]
 >(
+  socket: PartySocket | undefined,
   event: E,
-  callback: (
-    e: CbArgs extends Event ? WebSocketEventMap[E] : never
-  ) => CbArgs extends Event ? void : never
+  callback:
+    | ((
+        e: CbArgs extends Event ? WebSocketEventMap[E] : never
+      ) => CbArgs extends Event ? void : never)
+    | undefined
 ) => {
-  const socket = usePartySocket();
   const _callback = useRef(callback);
   _callback.current = callback;
 
   useEffect(() => {
-    socket.addEventListener(event, _callback.current);
+    if (!socket || !_callback.current) return;
+    const stableListener = (e: any) => _callback.current?.(e);
+    socket.addEventListener(event, stableListener);
     return () => {
-      socket.removeEventListener(event, _callback.current);
+      socket.removeEventListener(event, stableListener);
+    };
+  }, [socket]);
+};
+
+/**
+ *
+ * @param options when provided the hook will ignore it's context and use a global socket with the provided options
+ * @returns
+ */
+export const usePartySocket = ({
+  onOpen,
+  onClose,
+  onError,
+  onMessage,
+  ...options
+}: UsePartySocketOptions) => {
+  const socket =
+    useContext(PartySocketContext) ||
+    (!!options?.host ? getGlobalPartySocket(options) : undefined);
+
+  usePartySocketEvent(socket, "open", onOpen);
+  usePartySocketEvent(socket, "message", onMessage);
+  usePartySocketEvent(socket, "close", onClose);
+  usePartySocketEvent(socket, "error", onError);
+
+  useEffect(() => {
+    if (!socket) return;
+
+    const { host, room } = socket.partySocketOptions;
+    listenersCounter[host] ??= {};
+    listenersCounter[host][room] ??= 0;
+    listenersCounter[host][room] += 1;
+
+    if (socket.shouldReconnect) socket.reconnect();
+    return () => {
+      listenersCounter[host][room] -= 1;
+      // only close the socket if there are no more listeners
+      if (listenersCounter[host][room] === 0) socket?.close();
     };
   }, [socket]);
 
+  if (!socket)
+    throw new Error(
+      "usePartySocket must be used within a PartySocketProvider, or with a host provided"
+    );
+
   return socket;
 };
-
-export const usePartySocketMessage = (
-  callback: (e: WebSocketEventMap["message"]) => void
-) => usePartySocketEvent("message", callback);
