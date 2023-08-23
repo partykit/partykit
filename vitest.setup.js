e93b752aa7377854608e1196f8c227d3255aa0f3
Jani Eväkallio <jani.evakallio@gmail.com>
2023-08-23 13:28:57 +0100

    Fix tests

--- vitest.setup.js
+++ vitest.setup.js
@@ -2,3 +2,5 @@
 globalThis.PARTYKIT_API_BASE = "fuck.partykit.dev";
 // eslint-disable-next-line no-undef
 globalThis.PARTYKIT_DASHBOARD_BASE = "dashboard.partykit.io";
+// eslint-disable-next-line no-undef
+globalThis.CLERK_PUBLISHABLE_KEY = "do_not_access_clerk_in_tests";
