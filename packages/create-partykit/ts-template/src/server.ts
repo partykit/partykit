2d5fd8e334b7bc411a6501c779c56ef4e03fe637
Jani Eväkallio <jani.evakallio@gmail.com>
2023-08-31 18:30:58 +0100

    Classify npm create template

--- packages/create-partykit/ts-template/src/server.ts
+++ packages/create-partykit/ts-template/src/server.ts
@@ -1,40 +1,37 @@
 import type {
-  PartyKitConnection,
-  PartyKitContext,
-  PartyKitRoom,
-  PartyKitServer,
+  Party,
+  PartyConnection,
+  PartyConnectionContext,
+  PartyServer,
+  PartyWorker,
 } from "partykit/server";
 
-export default {
-  onConnect(
-    conn: PartyKitConnection,
-    room: PartyKitRoom,
-    ctx: PartyKitContext
-  ) {
+export default class Server implements PartyServer {
+  constructor(readonly party: Party) {}
+
+  onConnect(conn: PartyConnection, ctx: PartyConnectionContext) {
     // A websocket just connected!
     console.log(
       `Connected:
   id: ${conn.id}
-  room: ${room.id}
+  room: ${this.party.id}
   url: ${new URL(ctx.request.url).pathname}`
     );
 
     // let's send a message to the connection
     conn.send("hello from server");
+  }
 
-    // let's listen to all messages sent by the connection
-    conn.addEventListener("message", (event) => {
-      // let's log the message
-      console.log(
-        `connection ${conn.id} sent message: ${event.data as string}`
-      );
-      // as well as broadcast it to all
-      // the other connections in the room...
-      room.broadcast(
-        `${conn.id}: ${event.data as string}`,
-        // ...except for the connection it came from
-        [conn.id]
-      );
-    });
-  },
-} satisfies PartyKitServer;
+  onMessage(message: string, sender: PartyConnection) {
+    // let's log the message
+    console.log(`connection ${sender.id} sent message: ${message}`);
+    // as well as broadcast it to all the other connections in the room...
+    this.party.broadcast(
+      `${sender.id}: ${message}`,
+      // ...except for the connection it came from
+      [sender.id]
+    );
+  }
+}
+
+Server satisfies PartyWorker;
