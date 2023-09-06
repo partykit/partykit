8f46ff420798242a1d69de659b38c59f0a1f6a95
Sylwia Vargas <sylwia.vargas@gmail.com>
2023-09-06 13:42:21 +0100

    cleaning

--- apps/blog/astro.config.mjs
+++ apps/blog/astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  site: "https://blog.partykit.io", // replace this with your deployed domain
   integrations: [
     tailwind({
       config: {
