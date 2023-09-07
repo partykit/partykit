dda95fda520a74a49405073f5e955010921f15c4
Jani Eväkallio <jani.evakallio@gmail.com>
2023-09-07 12:35:20 +0100

    Team announcement blog post

--- apps/blog/tailwind.config.cjs
+++ apps/blog/tailwind.config.cjs
@@ -11,12 +11,6 @@ function withOpacity(variableName) {
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    // Remove the following screen breakpoint or add other breakpoints
-    // if one breakpoint is not enough for you
-    screens: {
-      sm: "640px",
-    },
-
     // Uncomment the following extend
     // if existing Tailwind color palette will be used
 
