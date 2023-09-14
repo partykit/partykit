03ffb9027f179008d410e97fbc01503cd0f777f6
Sunil Pai <threepointone@gmail.com>
2023-09-14 08:11:47 +0100

    add a changeset, pass typecheck

--- packages/create-partykit/ts-template/src/client.ts
+++ packages/create-partykit/ts-template/src/client.ts
@@ -4,7 +4,7 @@ import PartySocket from "partysocket";
 
 declare const PARTYKIT_HOST: string;
 
-let pingInterval: number;
+let pingInterval: ReturnType<typeof setInterval>;
 
 // Let's append all the messages we get into this DOM element
 const output = document.getElementById("app") as HTMLDivElement;
