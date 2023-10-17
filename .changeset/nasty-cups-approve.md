c57b51bd238dac0a0d4ca0d816c181242a730df8
Sunil Pai <threepointone@gmail.com>
2023-10-17 16:12:23 +0100

    update deps (#506)
    Of note, this fixes partymix, our remix adapter

---
+++ .changeset/nasty-cups-approve.md
@@ -0,0 +1,9 @@
+---
+"create-partykit": patch
+"partykit": patch
+"partymix": patch
+---
+
+update deps
+
+Of note, this fixes partymix, our remix adapter
--- examples/remix/app/routes/_index.tsx
+++ examples/remix/app/routes/_index.tsx
@@ -1,6 +1,6 @@
-import type { V2_MetaFunction } from "partymix";
+import type { MetaFunction } from "partymix";
 
-export const meta: V2_MetaFunction = () => {
+export const meta: MetaFunction = () => {
   return [
     { title: "New Partymix App" },
     { name: "description", content: "Welcome to Remix!" },
