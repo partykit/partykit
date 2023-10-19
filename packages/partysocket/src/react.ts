4dc11423a13a613771cf369958431abde65e795c
Jani Eväkallio <jani.evakallio@gmail.com>
2023-10-19 14:38:25 +0100

    Initial work towards reconnection logic that doesnt drop messages

--- packages/partysocket/src/react.ts
+++ packages/partysocket/src/react.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useMemo, useState, useRef } from "react";
 import type { PartySocketOptions } from ".";
 import PartySocket from ".";
 import useWebSocketImpl from "./use-ws";
@@ -10,50 +10,130 @@ type UsePartySocketOptions = PartySocketOptions & {
   onError?: (event: WebSocketEventMap["error"]) => void;
 };
 
+/** Updates options tha require the socket to reconnect */
+const useStableOptions = (options: PartySocketOptions) => {
+  const identity = JSON.stringify([
+    options.id,
+    options.host,
+    options.party,
+    options.path,
+    options.protocol,
+    options.protocols,
+    // NOTE: if query is defined as a function, the code
+    // won't reconnect when you change the function
+    options.query,
+  ]);
+
+  return useMemo(() => {
+    return options;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [identity]);
+};
+
 // A React hook that wraps PartySocket
 export default function usePartySocket(options: UsePartySocketOptions) {
   const { onOpen, onMessage, onClose, onError, ...partySocketOptions } =
     options;
 
-  const socketRef = useRef<PartySocket>(
-    new PartySocket({
-      ...partySocketOptions,
-      startClosed: true, // only connect on mount
-    })
+  const stableOptions = useStableOptions(partySocketOptions);
+
+  const [socket, setSocket] = useState<PartySocket>(
+    () => new PartySocket({ ...stableOptions, startClosed: true })
   );
+
+  const activeSocketRef = useRef<PartySocket | null>(null);
+  const activeHandlersRef = useRef<Partial<UsePartySocketOptions>>({});
+
+  useEffect(() => {
+    if (socket !== activeSocketRef.current) {
+      activeSocketRef.current = socket;
+      return;
+    }
+
+    const activeSocket = activeSocketRef.current;
+    const nextSocket = new PartySocket({
+      ...stableOptions,
+      startClosed: false,
+    });
+
+    nextSocket.addEventListener("open", () => {
+      activeSocket.close();
+    });
+
+    setSocket(nextSocket);
+
+    return () => nextSocket.close();
+  }, [socket, stableOptions]);
 
   // note: this effect must be defined before the auto-connect effect below
   useEffect(() => {
-    const socket = socketRef.current;
-    if (onOpen) socket.addEventListener("open", onOpen);
-    if (onClose) socket.addEventListener("close", onClose);
-    if (onError) socket.addEventListener("error", onError);
-    if (onMessage) socket.addEventListener("message", onMessage);
-
-    return () => {
-      if (onOpen) socket.removeEventListener("open", onOpen);
-      if (onClose) socket.removeEventListener("close", onClose);
-      if (onError) socket.removeEventListener("error", onError);
-      if (onMessage) socket.removeEventListener("message", onMessage);
+    const prevSocket = activeSocketRef.current;
+    const prevHandlers = activeHandlersRef.current;
+    const nextHandlers = {
+      onOpen,
+      onClose,
+      onError,
+      onMessage,
     };
-  }, [onOpen, onMessage, onClose, onError]);
+
+    if (nextHandlers.onOpen)
+      socket.addEventListener("open", nextHandlers.onOpen);
+    if (prevHandlers?.onOpen)
+      prevSocket?.removeEventListener("open", prevHandlers.onOpen);
+
+    if (nextHandlers.onClose)
+      socket.addEventListener("close", nextHandlers.onClose);
+    if (prevHandlers?.onClose)
+      prevSocket?.removeEventListener("close", prevHandlers.onClose);
+
+    if (nextHandlers.onError)
+      socket.addEventListener("error", nextHandlers.onError);
+    if (prevHandlers?.onError)
+      prevSocket?.removeEventListener("error", prevHandlers.onError);
+
+    if (nextHandlers.onMessage)
+      socket.addEventListener("message", nextHandlers.onMessage);
+    if (prevHandlers?.onMessage)
+      prevSocket?.removeEventListener("message", prevHandlers.onMessage);
+
+    activeSocketRef.current = socket;
+    activeHandlersRef.current = nextHandlers;
+
+    // return () => {
+    //   console.log("removing socket event listeners", socket.url);
+    //   if (onOpen) socket.removeEventListener("open", onOpen);
+    //   if (onClose) socket.removeEventListener("close", onClose);
+    //   if (onError) socket.removeEventListener("error", onError);
+    //   if (onMessage) socket.removeEventListener("message", onMessage);
+    // };
+  }, [socket, onOpen, onMessage, onClose, onError]);
 
   // note: this effect must be defined after the event listener registration above
   useEffect(
     () => {
-      const socket = socketRef.current;
-      if (options.startClosed !== true) {
+      if (
+        options.startClosed !== true &&
+        socket.readyState !== socket.OPEN &&
+        socket.readyState !== socket.CONNECTING
+      ) {
+        console.log("opening initial socket", socket.url);
         socket.reconnect();
       }
-
-      return () => {
-        socket.close();
-      };
+      //}
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    []
+    [socket]
   );
-  return socketRef.current;
+
+  useEffect(() => {
+    // when the socket reference changed,
+    // or we're unmounting the hook, close
+    return () => {
+      activeSocketRef.current?.close();
+    };
+  }, []);
+
+  return socket;
 }
 
 export { useWebSocketImpl as useWebSocket };
