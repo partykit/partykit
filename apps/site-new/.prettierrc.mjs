bbdb6bf4eb495c78fe44b8f6fabf2fce72541fe6
Matt Webb <matt@interconnected.org>
2023-11-09 11:43:28 +0000

    [WIP] Site development (#547)
    * Site skeleton
    
    * Proof of concept build
    
    * Layout fix
    
    * fix lint errors
    
    ---------
    
    Co-authored-by: Sunil Pai <threepointone@gmail.com>

---
+++ apps/site-new/.prettierrc.mjs
@@ -0,0 +1,13 @@
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
