b388e3e563af9b314e3ab095e2efe566492a93aa
Sunil Pai <threepointone@gmail.com>
2023-11-13 16:16:49 +0000

    fetch blog entries
    This fetches blog post entries from the blog's rss feed and writes it to disk as json, so we can consume it and render it on the homepage.

---
+++ apps/site-new/scripts/fetch-blog-entries.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+import xml2json from "xml2json";
+import fs from "fs";
+
+async function fetchBlogEntries() {
+  const url = "https://blog.partykit.io/rss.xml";
+  const response = await fetch(url);
+  const xml = await response.text();
+  const json = xml2json.toJson(xml);
+  const data = JSON.parse(json);
+  fs.writeFileSync(
+    "src/blog.json",
+    JSON.stringify(data.rss.channel.item, null, 2) + "\n",
+  );
+}
+
+fetchBlogEntries().catch(console.error);
