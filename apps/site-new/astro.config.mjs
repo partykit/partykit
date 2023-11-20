ace6fcc93ade6722de4e50bb4e96f5960b64224c
Matt Webb <matt@interconnected.org>
2023-11-20 10:48:44 +0000

    Adds mdx pages

--- apps/site-new/astro.config.mjs
+++ apps/site-new/astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [mdx(), react(), tailwind()],
 });
