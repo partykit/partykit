5af37949faede6a8c98341b8e02e452d23abe8cb
ThinkMake <tech@thinkm.studio>
2023-11-29 16:48:44 +0000

    core typography, header and footer

--- apps/blog/src/components/Datetime.tsx
+++ apps/blog/src/components/Datetime.tsx
@@ -8,19 +8,9 @@ export interface Props {
 
 export default function Datetime({ datetime, size = "sm", className }: Props) {
   return (
-    <div className={`flex items-center opacity-80 ${className}`}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className={`${
-          size === "sm" ? "scale-90" : "scale-100"
-        } relative top-0.5 inline-block h-6 w-6 fill-skin-base`}
-        aria-hidden="true"
-      >
-        <path d="M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0 4h2v2h-2zm4-4h2v2h-2zm0 4h2v2h-2z"></path>
-        <path d="M5 22h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zM19 8l.001 12H5V8h14z"></path>
-      </svg>
+    <div>
       <span className="sr-only">Posted on:</span>
-      <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
+      <span>
         <FormattedDatetime datetime={datetime} />
       </span>
     </div>
@@ -44,9 +34,6 @@ const FormattedDatetime = ({ datetime }: { datetime: string | Date }) => {
   return (
     <>
       {date}
-      <span aria-hidden="true"> | </span>
-      <span className="sr-only">&nbsp;at&nbsp;</span>
-      {time}
     </>
   );
 };
