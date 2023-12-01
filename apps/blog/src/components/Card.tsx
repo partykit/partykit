c440c1554cfa174fd5a254fc34d967c667a50d51
Mark Hurrell | ThinkMake Studio <55025702+ThinkMake@users.noreply.github.com>
2023-12-01 17:43:52 +0000

    Blog reskin (#585)
    * core typography, header and footer
    
    * more reskinning the blog template
    
    remaining:
    - headers across the index pages
    - responsive views
    
    * fixed navigation in mobile views
    
    * mobile viewports
    
    * tablet layouts
    
    * increase body font size on mobile viewports
    
    also, fix blog post card format on larger/mobile viewports
    
    * fix undefined class on blog homepage button
    
    * make the footer font weight consistent with homepage
    
    * move css include to top of head
    
    remove about.md
    
    * maybe fixes the button issue
    
    * remove unused props
    
    * another lint fix
    
    * Linting and copy
    
    * Copy
    
    ---------
    
    Co-authored-by: Sunil Pai <threepointone@gmail.com>
    Co-authored-by: Matt Webb <matt@interconnected.org>

--- apps/blog/src/components/Card.tsx
+++ apps/blog/src/components/Card.tsx
@@ -10,17 +10,17 @@ export interface Props {
 export default function Card({ href, frontmatter, secHeading = true }: Props) {
   const { title, pubDatetime, description } = frontmatter;
   return (
-    <li className="my-6">
+    <li className="card">
       <a
         href={href}
-        className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
+        className="font-L"
       >
         {secHeading ? (
-          <h2 className="text-lg font-medium decoration-dashed hover:underline">
+          <h2 className="">
             {title}
           </h2>
         ) : (
-          <h3 className="text-lg font-medium decoration-dashed hover:underline">
+          <h3 className="">
             {title}
           </h3>
         )}
