4c0fa9ae15f637c7578d4318d79fcc3a9060c628
ayoubqrt <ayoub.mstk@gmail.com>
2023-12-03 10:52:19 +0100

    warnings

--- packages/partysocket/src/tests/partysocket.test.ts
+++ packages/partysocket/src/tests/partysocket.test.ts
@@ -2,28 +2,11 @@
  * @vitest-environment jsdom
  */
 
-import {
-  beforeEach,
-  afterEach,
-  test,
-  expect,
-  vitest,
-  beforeAll,
-  afterAll,
-} from "vitest";
+import { test, expect } from "vitest";
 import PartySocket from "../index";
+
 const PORT = 50123;
 const URL = `localhost:${PORT}/`;
-
-let wss: PartySocket;
-
-beforeAll(() => {
-  wss = new PartySocket({ host: URL });
-});
-
-afterEach(() => {
-  vitest.restoreAllMocks();
-});
 
 test("throws if host or room is not set", () => {
   expect(() => {
@@ -35,5 +18,6 @@ test("throws if host or room is not set", () => {
 test("if the room is correctly updated after calling updateProperties", () => {
   const partySocket = new PartySocket({ host: URL, room: "first-room" });
   partySocket.updateProperties({ room: "second-room" });
+
   expect(partySocket.room).toBe("second-room");
 });
