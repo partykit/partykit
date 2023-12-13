5d795d93bada85ac5c100b45214914d321a13bc3
Sunil Pai <threepointone@gmail.com>
2023-12-13 17:21:38 +0000

    remove `persist: true` in y-partykit examples

--- examples/monaco/src/server.ts
+++ examples/monaco/src/server.ts
@@ -2,8 +2,6 @@ import type { PartyKitServer } from "partykit/server";
 import { onConnect } from "y-partykit";
 export default {
   onConnect(ws, room) {
-    return onConnect(ws, room, {
-      persist: true,
-    });
+    return onConnect(ws, room);
   },
 } satisfies PartyKitServer;
