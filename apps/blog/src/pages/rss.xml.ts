463f44cdc2e55fe2a5f344148b73ecdb4fda95e8
Sunil Pai <threepointone@gmail.com>
2023-12-24 17:04:34 +0000

    Update rss.xml.ts (#646)

--- apps/blog/src/pages/rss.xml.ts
+++ apps/blog/src/pages/rss.xml.ts
@@ -4,7 +4,7 @@ import getSortedPosts from "@utils/getSortedPosts";
 import slugify from "@utils/slugify";
 import { SITE } from "@config";
 
-export async function get() {
+export async function GET() {
   const posts = await getCollection("blog");
   const sortedPosts = getSortedPosts(posts);
   return rss({
