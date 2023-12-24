20845a8e013d9f7fc2947f6228f674fe05d08fc5
Sunil Pai <threepointone@gmail.com>
2023-12-24 16:22:20 +0000

    update site deps

--- apps/site/astro.config.mjs
+++ apps/site/astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
