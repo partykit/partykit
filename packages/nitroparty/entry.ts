d8cc512ece82f6017e63f43aabaa994600228a82
Sunil Pai <threepointone@gmail.com>
2023-12-28 15:10:01 +0000

    nitroparty: pass context and body to request handler

--- packages/nitroparty/entry.ts
+++ packages/nitroparty/entry.ts
@@ -6,17 +6,36 @@ import type * as Party from "partykit/server";
 // @ts-expect-error polyfilled by nitro
 const nitroApp = useNitroApp();
 
+const METHOD_WITH_BODY_RE = /post|put|patch/i;
+
+function requestHasBody(request: Party.Request): boolean {
+  return METHOD_WITH_BODY_RE.test(request.method);
+}
+
 export default class PartyServer implements Party.Server {
-  static onFetch(request: Party.Request) {
+  static async onFetch(
+    request: Party.Request,
+    lobby: Party.FetchLobby,
+    ctx: Party.ExecutionContext
+  ) {
     const url = new URL(request.url);
+
+    let body;
+    if (requestHasBody(request)) {
+      body = Buffer.from(await request.arrayBuffer());
+    }
     return nitroApp.localFetch(url.pathname + url.search, {
-      context: {},
+      context: {
+        request,
+        lobby,
+        ctx,
+      },
       host: url.hostname,
       protocol: url.protocol,
       method: request.method,
       // we need to cast the headers to a web standard Headers
       headers: request.headers as unknown as Headers,
-      body: undefined,
+      body,
     });
   }
 }
