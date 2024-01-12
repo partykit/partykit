2b20b154816430b714138f45b4348af909f8b91d
Sunil Pai <threepointone@gmail.com>
2024-01-12 10:08:44 +0000

    Update dependencies

--- examples/remix/remix.config.js
+++ examples/remix/remix.config.js
@@ -11,15 +11,4 @@ export default {
   assetsBuildDirectory: "public/dist",
   serverBuildPath: "dist/index.js",
   publicPath: "/dist/",
-  future: {
-    v2_dev: true,
-    v2_errorBoundary: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-  },
-  serverNodeBuiltinsPolyfill: {
-    modules: {},
-  },
 };
