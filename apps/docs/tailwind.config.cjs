0a0322c9e3bc7bb597002eaa562b8c573e70c168
Daniel <70210356+Itsnotaka@users.noreply.github.com>
2024-02-19 10:24:48 +0800

    style: added missing starlight tailwind dependency

--- apps/docs/tailwind.config.cjs
+++
@@ -1,8 +0,0 @@
-// tailwind.config.cjs
-const starlightPlugin = require("@astrojs/starlight-tailwind");
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  plugins: [starlightPlugin()]
-};
