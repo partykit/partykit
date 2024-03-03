f15d9e20e9f4ebd5bccaf5a9d2deb7819a3095ba
Sunil Pai <threepointone@gmail.com>
2024-03-03 16:13:16 +0000

    fix: default import from node: builtins should work (#834)

--- examples/postgres/src/server.ts
+++ examples/postgres/src/server.ts
@@ -5,17 +5,17 @@ import postgres from "postgres";
 
 const connectionString = process.env.DB_CONNECTION_URL as string;
 
-const client = postgres(connectionString, { prepare: false });
-const db = drizzle(client);
-
-export const users = pgTable("users", {
-  id: serial("id").primaryKey(),
-  fullName: text("full_name"),
-  phone: varchar("phone", { length: 256 })
-});
-
 export default {
   async onFetch() {
+    const client = postgres(connectionString, { prepare: false });
+    const db = drizzle(client);
+
+    const users = pgTable("users", {
+      id: serial("id").primaryKey(),
+      fullName: text("full_name"),
+      phone: varchar("phone", { length: 256 })
+    });
+
     const allUsers = await db.select().from(users);
     return new Response(JSON.stringify(allUsers), {
       headers: { "content-type": "application/json" }
